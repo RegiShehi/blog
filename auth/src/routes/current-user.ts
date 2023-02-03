de2d410a0de5cfde22ee3fea08e45fb37861008c
Regi Shehi <regi_shehi@hotmail.com>
2023-02-03 12:30:44 +0100

    Add authentication

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,13 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
+
+import { currentUser } from '../middlewares/current-user';
+import { requireAuth } from '../middlewares/require-auth';
 
 const router = express.Router();
 
-router.get('/api/users/currentuser', (req, res) => {
-  res.send('Hello!');
+router.get('/api/users/currentuser', currentUser, requireAuth, (req, res) => {
+  res.send({ currentUser: req.currentUser });
 });
 
 export { router as currentUserRouter };
