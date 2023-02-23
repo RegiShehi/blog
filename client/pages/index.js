a01cb103072b88aa86db72566ef9fc65ae435bb4
Regi Shehi <regi_shehi@hotmail.com>
2023-02-23 19:02:54 +0100

    Add sign and sign up page

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,16 @@
-export default () => {
-    return <h1>Landing page</h1>
-}
+import buildClient from '../api/build-client';
+
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
+
+  return <h1>{currentUser ? 'You are signed in' : 'You are not signed in'}</h1>;
+};
+
+LandingPage.getInitialProps = async (context) => {
+  const client = buildClient(context);
+  const { data } = await client.get('/api/users/currentuser');
+
+  return data;
+};
+
+export default LandingPage;
