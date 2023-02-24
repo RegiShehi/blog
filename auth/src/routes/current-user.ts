980c95acea97b18ac22184fb4476b4334b78eb63
Regi Shehi <regi_shehi@hotmail.com>
2023-02-24 18:40:05 +0100

    Import errors and middlewares from common module

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,7 +1,5 @@
 import express from 'express';
-
-import { currentUser } from '../middlewares/current-user';
-import { requireAuth } from '../middlewares/require-auth';
+import { currentUser } from '@rstickets70/common';
 
 const router = express.Router();
 
