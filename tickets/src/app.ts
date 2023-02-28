b7869abc2f04f76aacd9a2b018591ca0573265be
Regi Shehi <regi_shehi@hotmail.com>
2023-02-28 20:45:04 +0100

    Add update ticket route

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -6,6 +6,8 @@ import { json } from 'body-parser';
 import { errorHandler, NotFoundError, currentUser } from '@rstickets70/common';
 import { createTicketRouter } from './routes/new';
 import { showTicketRouter } from './routes/show';
+import { indexTicketRouter } from './routes';
+import { updateTicketRouter } from './routes/update';
 
 const app = express();
 app.set('trust proxy', true); //trust traffic behind ingress nginx proxy
@@ -21,6 +23,8 @@ app.use(
 app.use(currentUser);
 app.use(createTicketRouter);
 app.use(showTicketRouter);
+app.use(indexTicketRouter);
+app.use(updateTicketRouter);
 
 app.all('*', async (req, res, next) => {
   throw new NotFoundError();
