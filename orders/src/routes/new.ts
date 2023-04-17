c42a6f83287b8cfeec5b457de5c85b6c02394e0a
Regi Shehi <regi_shehi@hotmail.com>
2023-04-17 20:53:43 +0200

    Add ticket and order models to Order service

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,9 +1,17 @@
 import express, { Request, Response } from 'express';
+import { requireAuth, validateRequest } from '@rstickets70/common';
+import { body } from 'express-validator';
 
 const router = express.Router();
 
-router.post('/api/orders', async (req: Request, res: Response) => {
-  res.send({});
-});
+router.post(
+  '/api/orders',
+  requireAuth,
+  [body('ticketId').not().isEmpty().withMessage('Ticket id must be provided')],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    res.send({});
+  }
+);
 
 export { router as newOrderRouter };
